5ed1f4aa8e6faf4fd1f4e0615f4bc5333a339359
Fernando01santnaai <cool.fernadno23@gmail.com>
2020-10-31 10:50:03 -0300

    adicionando tela de curriculo

--- database/updates_aluno/curriculo.php
+++ database/updates_aluno/curriculo.php
@@ -2,10 +2,23 @@
 include('../conection.php');
 session_start();
 //escolaridade
-$nivel_escola = $_POST['nivel_escola'];
-$instituicao = $_POST['instituicao'];
-$ano_conclusao = $_POST['ano_conclusao'];
-$periodo = $_POST['periodo'];
+
+
+
+//pegando id do usuario ativo fazendo uma pesquisa no banco
+$email = $_SESSION['user'];
+try {
+    //validar pelo email ou identificador
+   $consulta = $pdo->prepare("SELECT id FROM alunos WHERE email = :email");
+   $consulta->bindValue(':email',$email);
+   $consulta->execute();
+   $row = $consulta->fetchAll();
+   $id = $row[0]['id'];
+   echo $id;
+
+}catch (PDOException $th) {
+   echo $th;
+}
 
 //pegando valor dos radios de encino medio
 $progresso_curso ='';
@@ -20,63 +33,73 @@ if(isset( $_POST['radio_encino']) === "incompleto"){
         }
 }
 }
-//pegando valor de idioma e nivel de idioma
-     $idioma = $_POST['idioma'];
-     $nivel_idioma = $_POST['nivel_idioma'];
-     $qtdeEntidade = '';
-     $data_inicio_conhecimento = '';
-     $data_termino_conhecimento = '';
 
-     //adicionando ao banco os dados com array dos cursos, todos convertidos em json
-if (isset($_POST['qtdeCurso']) && isset($_POST['qtdeEntidade']) && isset($_POST['data_inicio_conhecimento'])&& isset($_POST['data_termino_conhecimento']) ) {
-     $qtdeEntidade = $_POST['qtdeCurso'];
-     $data_inicio_conhecimento = $_POST['data_inicio_conhecimento'];
-     $data_termino_conhecimento = $_POST['data_termino_conhecimento'];  
-     $entidade_curso = $_POST['qtdeEntidade'];
-     $email = $_SESSION['user'];
-
-     $entidade = json_encode($entidade_curso);
-     $cursos = json_encode($qtdeEntidade);
-     $data_inicio = json_encode($data_inicio_conhecimento);
-     $data_fim = json_encode($data_termino_conhecimento);
-     echo $cursos;
-
-     try {
-        $consulta = $pdo->prepare("UPDATE curriculos SET curso = :curso,data_inicio_curso = :data_inicio,data_termino_curso = :data_termino,entidade_curso = :entidade_curso WHERE id = :id");
-        $consulta->bindValue(':curso',$cursos);
-        $consulta->bindValue(':data_inicio',$data_inicio);
-        $consulta->bindValue(':data_termino',$data_fim);
-        $consulta->bindValue(':entidade_curso',$entidade);
-        $consulta->bindValue(':id',1);
-        $consulta->execute();
-    }catch (PDOException $th) {
-        echo $th;
-    }
-
-}
 
 /*
 mandar dados para o banco usando PDO
 fazer busca no banco e pegar o id do aluno para inserir na chave primaria
 */
-$usuario = $_SESSION['user'];
-$senha = $_SESSION['pass'];
+/*
 echo $usuario;
 try {
     $consulta = $pdo->prepare("SELECT * FROM alunos WHERE email = :email");
     $consulta->bindValue(':email',$usuario);
     $consulta->execute();
     $dados_aluno = $consulta->fetchAll();
-    $id_aluno = $dados_aluno{0}['id'];
+    $id_aluno = $dados_aluno[0]['id'];
    // echo $id_aluno;
 }catch (PDOException $th) {
     echo $th;
-}
+}*/
+//pegando valores da empresa e transformando em json
+     //adicionando ao banco os dados com array dos cursos, todos convertidos em json
+     $data_inicio_empresa = '';
+        $data_termino_empresa = '';
+     if (isset($_POST['exp_empresa']) && isset($_POST['cargos']) && isset($_POST['data_inicio_empresa'])&& isset($_POST['data_termino_empresa']) ) {
+        $exp_empresa = $_POST['qtdeCurso'];
+        $data_inicio_conhecimento = $_POST['data_inicio_conhecimento'];
+        $data_termino_conhecimento = $_POST['data_termino_conhecimento'];  
+        $entidade_curso = $_POST['qtdeEntidade'];
+       
+   
+        $exp_empresa = json_encode($entidade_curso);
+        $cargos = json_encode($qtdeEntidade);
+        $data_inicio_empresa = json_encode($data_inicio_conhecimento);
+        $data_termino_empresa = json_encode($data_termino_conhecimento);
+     }else{
+        $exp_empresa = '';
+        $cargos = '';
+        $data_inicio_empresa = '';
+        $data_termino_empresa = '';
+     }
+        $nivel_escola = $_POST['nivel_escola'];
+        $instituicao = $_POST['instituicao'];
+        $ano_conclusao = $_POST['ano_conclusao'];
+        $periodo = $_POST['periodo'];
+        //pegando valor de idioma e nivel de idioma
+        $idioma = $_POST['idioma'];
+        $nivel_idioma = $_POST['nivel_idioma'];
+        $qtdeEntidade = '';
+        $data_inicio_conhecimento = '';
+        $data_termino_conhecimento = '';
 try {
-    $stmt = $pdo->prepare("UPDATE curriculo SET id = :id, id_aluno = :id_aluno, nivel = :nivel, entidade = :entidade,estado = :estado, ano_conclusao = :ano_conclusao, periodo = :periodo, idioma = :idioma, nivel_idioma:nivel_idioma,emrpesa = :empresa,cargo = :cargo,data_inicio_empresa = :data_inicio_empresa,data_termino_empresa = :data_empresa_curso  WHERE cpf = :cpf");
+    $stmt = $pdo->prepare("INSERT INTO curriculos SET  
+    id_aluno = :id_aluno, 
+    nivel = :nivel, 
+    entidade = :entidade,
+    estado = :estado, 
+    ano_conclusao = :ano_conclusao,
+    periodo = :periodo, 
+    idioma = :idioma,
+    nivel_idioma = :nivel_idioma,
+    empresa = :empresa,
+    cargo = :cargo,
+    data_inicio_empresa = :data_inicio_empresa,
+    data_termino_empresa = :data_empresa_curso 
+    WHERE id = :id");
 
-    $stmt->bindValue(':id',null);
-    $stmt->bindValue(':id_curriculo',$id_aluno);
+
+    $stmt->bindValue(':id_aluno',$id);
     $stmt->bindValue(':nivel',$nivel_escola);
     $stmt->bindValue(':entidade',$instituicao);
     $stmt->bindValue(':estado',$progresso_curso);
@@ -84,16 +107,43 @@ try {
     $stmt->bindValue(':periodo',$periodo);
     $stmt->bindValue(':idioma',$idioma);
     $stmt->bindValue(':nivel_idioma',$nivel_idioma);
-    $stmt->bindValue(':entidade_curso',$entidade_curso);
-    $stmt->bindValue(':data_inicio_curso',$data_inicio_curso);
-    $stmt->bindValue(':data_termino_curso',$data_termino_curso);
-    $stmt->bindValue(':empresa',$empresa);
-    $stmt->bindValue(':cargo',$cargo);
+    $stmt->bindValue(':empresa',$exp_empresa);
+    $stmt->bindValue(':cargo',$cargos);
     $stmt->bindValue(':data_inicio_empresa',$data_inicio_empresa);
-    $stmt->bindValue(':cpf',$cpf);
+    $stmt->bindValue(':data_inicio_empresa',$data_termino_empresa);
+    $stmt->bindValue(':id',$id);
     $stmt->execute();
 } catch (\Throwable $th) {
     echo $th->getMessage();
 }
 
-?>
+
+
+     //adicionando ao banco os dados com array dos cursos, todos convertidos em json
+     if (isset($_POST['qtdeCurso']) && isset($_POST['qtdeEntidade']) && isset($_POST['data_inicio_conhecimento'])&& isset($_POST['data_termino_conhecimento']) ) {
+        $qtdeEntidade = $_POST['qtdeCurso'];
+        $data_inicio_conhecimento = $_POST['data_inicio_conhecimento'];
+        $data_termino_conhecimento = $_POST['data_termino_conhecimento'];  
+        $entidade_curso = $_POST['qtdeEntidade'];
+       
+   
+        $entidade = json_encode($entidade_curso);
+        $cursos = json_encode($qtdeEntidade);
+        $data_inicio = json_encode($data_inicio_conhecimento);
+        $data_fim = json_encode($data_termino_conhecimento);
+        echo $cursos;
+   
+        try {
+            //validar pelo email ou identificador
+           $consulta = $pdo->prepare("UPDATE curriculos SET curso = :curso,data_inicio_curso = :data_inicio,data_termino_curso = :data_termino,entidade_curso = :entidade_curso WHERE id = :id");
+           $consulta->bindValue(':curso',$cursos);
+           $consulta->bindValue(':data_inicio',$data_inicio);
+           $consulta->bindValue(':data_termino',$data_fim);
+           $consulta->bindValue(':entidade_curso',$entidade);
+           $consulta->bindValue(':id',$id);
+           $consulta->execute();
+       }catch (PDOException $th) {
+           echo $th;
+       }
+   
+   }
---
+++ views/curriculo/curriculo.php
@@ -0,0 +1,152 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+
+    <title>Hello, world!</title>
+    <style>
+      body{
+        background: #DCDCDC;
+      }
+      .curriculo{
+        padding-left:2rem;
+        padding-right:2rem;
+        padding-top:1rem;
+        padding-bottom:1rem;
+        margin-top:3rem;
+        border-radius:10px;
+        background: white;
+      }
+    </style>
+  </head>
+  <body>
+ 
+    <div class="container">
+      
+            <form class="curriculo" method="POST" action="../../../database/updates_aluno/curriculo.php">
+                <h5 class="titulo-black">Escolaridade</h5>
+                <div class="row">
+                  <div class="col-12 sucesso-envio">
+
+                  </div>
+                </div>
+                <div class="row">
+                  <div class="col-6">
+                    <label>Nivel de ensino</label>
+                    <select name="nivel_escola" id="nivel_escola" name="nivel_escola" class="form-control">
+                      <option value="0">Selecione</option>
+                      <option value="medio">Médio</option>
+                      <option value="fundamental">Fundamental</option>
+                      <option value="superior">Superior</option>
+                    </select>
+                  </div>
+                  <div class="col-6">
+                    <label>Entidade</label>
+                    <input type="text" name="instituicao" id="entidade_escola" class="form-control" placeholder="entidade">
+                  </div>
+                </div>
+                <div class="row ">
+                  <div class="col-12">
+                    <label>Selecione o progresso da sua formação:</label><br>
+                    <input type="radio" name="radio_encino" id="radio1" value="incompleto">
+                    <label>Incompleto</label>
+
+                    <input type="radio" name="radio_encino" id="radio2" value="completo">
+                    <label>Completo</label>
+
+                    <input type="radio" name="radio_encino" id="radio3" value="cursando">
+                    <label>Cursando</label>
+                  </div>
+                </div>
+                <div class="row periodo">
+                  <div class='col-6'>
+                    <label for='ano de conclusao'>Ano de conclusao</label>
+                    <input type='date' name='ano_conclusao' class='form-control' id='ano_conclusao'>
+                  </div>
+                  <div class='col-6'>
+                    <label for='periodo'>Periodo</label>
+                    <select name='periodo' class='form-control' id='periodo'>
+                      <option value='0'>Selecione</option>
+                      <option value='1'>1º ano</option>
+                      <option value='2'>2º ano</option>
+                      <option value='3'>3º ano</option>
+                      <option value='4'>4º ano</option>
+                      <option value='5'>5º ano</option>
+                      <option value='6'>6º ano</option>
+                    </select>
+                  </div>
+                </div>
+                <h5 class="titulo-black">Idioma</h5>
+                <div class="row">
+
+                  <div class="col-6">
+                    <label for="">Idioma</label>
+                    <select name="idioma" id="idioma" class="form-control">
+                      <option value="0">Selecione</option>
+                      <option value="ingles">Ingles</option>
+                      <option value="Espanhol">Esánhol</option>
+                      <option value="Frances">Francês</option>
+                    </select>
+                  </div>
+                  <div class="col-6">
+                    <label for="">Nivel</label>
+                    <select name="nivel_idioma" id="nivel_idioma" class="form-control">
+                      <option value="0">Selecione</option>
+                      <option value="ingles">Basico</option>
+                      <option value="Espanhol">Intermediario</option>
+                      <option value="Frances">Avançado</option>
+                    </select>
+                  </div>
+                </div>
+
+                <h5 class="titulo-black">Conhecimentos</h5>
+                <div class="curso-adicional"></div>
+                <div class="row">
+                  <div class="col-12">
+                  <button type="button" class="btn btn-info btn-block add-curso">adicionar curso</button>
+                  </div>
+                </div>
+
+
+
+                <h5 class="titulo-black">Experiencia profissional</h5>
+                <div class="row">
+                  <div class="col-12">
+                    <label for="">Empresa</label>
+                    <input type="text" class="form-control" name="empresa" id="exp_empresa[]">
+                  </div>
+                </div>
+                <div class="row">
+                  <div class="col-12">
+                    <label for="">cargo</label>
+                    <input type="text" name="cargos[]" id="exp_cargo" class="form-control">
+                  </div>
+                </div>
+                <div class="row">
+                  <div class="col-6">
+                    <label for="">data de inicio</label>
+                    <input type="date" name="data_inicio_empresa" id="data_inicio_empresa[]" class="form-control">
+                  </div>
+                  <div class="col-6">
+                    <label for="">data de termino</label>
+                    <input type="date" name="data_termino_empresa" id="data_termino_empresa[]" class="form-control">
+                  </div>
+                </div>
+                <div class="new-exp">
+
+                </div>
+                <button type="button" class="btn btn-info btn-block add-exp">adicionar outra</button>
+                <button type="submit" class="btn btn-primary btn-block" id="atualizar_curriculo">atualizar</button>
+                <button type="button" class="btn btn-danger btn-block">Voltar</button>
+              </form>
+        </div>
+  
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
+  </body>
+</html>
